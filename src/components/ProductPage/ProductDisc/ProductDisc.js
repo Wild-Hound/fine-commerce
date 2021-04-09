7be43b4fe123b2680a2f93aac3aa11cd260b7852
Hound404 <wildhound404@gmail.com>
2021-04-09 22:55:03 +0600

    product disc partail done

--- src/components/ProductPage/ProductDisc/ProductDisc.js
+++ src/components/ProductPage/ProductDisc/ProductDisc.js
@@ -1,31 +1,41 @@
 import React from 'react'
+import 'antd/dist/antd.css';
+import { InputNumber } from 'antd';
 
 function ProductDisc({img,name,price,category = 'N/A',disc}) {
+
+    function onChange(value) {
+    console.log('changed', value);
+    }
+
     return (
         <div className='discBody'>
             <img src={img} className='discImage'/>
             <div>
                 <div className='discMeta'>
-                    <h2>{name}</h2>
+                    <h2 className='discTitle'>{name}</h2>
                     <div>
-                        <h3>Price: {price}</h3>
-                        <h3>Category: {category}</h3>
+                        <h3 >Price: <span className='discPrice'>{price}</span></h3>
+                        <h3 >Category: <span className='discCategory'>{category}</span></h3>
                     </div>
                 </div>
-                <div className='disc'>
-                    <p>{disc}</p>
+
+                    <p className='disc'>{disc}</p>
                     <div className='discQuantity'>
-                    <input 
-                    type='text' 
-                    placeholder='Quantity'
-                    />
-                    <button>Buy</button>
-                </div>
-                </div>
+                        <InputNumber min={1} max={10} defaultValue={1} onChange={onChange} />
+                        <button id='buyBtn'>Buy</button>
+                    </div>
+
                 
             </div>
         </div>
     )
 }
+
+
+
+
+
+
 
 export default ProductDisc
